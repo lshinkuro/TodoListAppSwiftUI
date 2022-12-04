1f5b2855f205ad7c67034fc04257338f369045cc
nurkholis <nur.kholis@ist.id>
2022-12-04 08:48:18 +0700

    Initial Commit

---
+++ Todo-List-App/Todo_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_AppApp.swift
+//  Todo-List-App
+//
+//  Created by nur kholis on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
